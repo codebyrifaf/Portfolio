aed4d46bf4849619c1169643a0e1bdc24189b1a9
RIFAF RAHMAN <127125526+codebyrifaf@users.noreply.github.com>
2025-07-18 11:11:31 +0600

    Update

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -84,7 +84,7 @@ export const HeroSection = () => {
         
         <div className="flex flex-col md:flex-row justify-center items-center mt-8 gap-4 z-10 relative">
           <a
-            href="https://drive.google.com/file/d/1jV2hiLwrkIrCIneVxiHTJFvNTNcUH-BN/view?usp=sharing"
+            href="https://drive.google.com/file/d/1VN4KX9ImnBe10fQWWIqaCfnP0NlhpTYz/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl font-semibold transition hover:bg-white/10"
